1d8ff3caaa79dd38f9a153229e775645a239c7a6
KroLik99 <isabelle.ebach@gmail.com>
2023-04-24 14:22:57 +0200

    do collatz

---
+++ src/AB01/AB411_02_collatz.java
@@ -0,0 +1,55 @@
+package AB01;
+
+public class AB411_02_collatz {
+    long resultProceduralAlgorithm;
+    long resultFunctionalAlgorithm;
+    long a;
+
+
+    private void collatzProceduralAlgorithm(long n) {
+        // Enthält Iterationen (while oder for)
+        long number = n;
+        while (number != 1) {
+            if (number % 2 == 0) {
+                number = number / 2;
+            } else {
+                number = 3 * number + 1;
+            }
+            System.out.println(number);
+        }
+        resultProceduralAlgorithm = number;
+    }
+
+    private void collatzFunctionalAlgorithm(long n) {
+        // Keine Iterationen oder lokale Variabeln
+        if (n > 0) {
+            if (n % 2 == 0) {
+                resultProceduralAlgorithm = n / 2;
+                n = n / 2;
+            } else {
+                resultProceduralAlgorithm = 3 * n + 1;
+                n = 3 * n + 1;
+            }
+            System.out.println(n);
+        }
+        if (n != 1) {
+            collatzProceduralAlgorithm(n);
+        }
+    }
+
+    public long getResultProceduralAlgorithm() {
+        return resultProceduralAlgorithm;
+    }
+
+    public void calculateProceduralAlgorithm(long n) {
+        collatzProceduralAlgorithm(n);
+    }
+
+    public long getResultFunctionalAlgorithm() {
+        return resultFunctionalAlgorithm;
+    }
+
+    public void calculateFunctionalAlgorithm(long resultFunctionalAlgorithm) {
+        collatzFunctionalAlgorithm(resultFunctionalAlgorithm);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import AB01.AB411_02_collatz;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int inputNumber;
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter an integer that is greater than 1: ");
+        String input = scanner.nextLine();
+        inputNumber = Integer.parseInt(input);
+        AB411_02_collatz a = new AB411_02_collatz();
+        //a.calculateProceduralAlgorithm(inputNumber);
+        a.calculateFunctionalAlgorithm(inputNumber);
+
+
     }
 }
